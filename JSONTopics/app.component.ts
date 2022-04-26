bcee8c4163fb23fb311407d2447b96e4732a7c9a
niitagile <43024343+niitagile@users.noreply.github.com>
2022-04-26 17:33:34 +0530

    Add files via upload

--- JSONTopics/app.component.ts
+++ JSONTopics/app.component.ts
@@ -1,6 +1,6 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import studentsData from './students.json';
-    
+import { HttpClient } from "@angular/common/http";    
 interface Student {  
     id: Number;  
     name: String;  
@@ -12,7 +12,13 @@ interface Student {
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
+export class AppComponent implements OnInit{
   title = 'jsonexample';
-  students: Student[] = studentsData;  
+  students: Student[] = studentsData;
+  product: any=[];
+  constructor(private httpClient: HttpClient) {} 
+  ngOnInit(): void {
+    this.httpClient.get("assets/product.json").subscribe(data=> {this.product=data;});
+  }
+
 }
